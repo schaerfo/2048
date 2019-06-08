ce5696f858042feed502b8ebc630ae325128a93d
Christian Schärf <c.schaerf@t-online.de>
2019-06-08 21:19:50 +0200

    Changed iteration in show_grid from index-based to pointer-based.

--- ui.c
+++ ui.c
@@ -7,15 +7,15 @@ void show_grid(grid_t ** grid, grid_size_t size) {
     for (grid_size_t i=0; i<size-1; ++i)
         printf("───┬");
     printf("───┐");
-    for (grid_size_t i=0; i<size; ++i) {
+    for (grid_t **i = grid; i<&(grid[size]); ++i) {
         printf("\n│");
-        for (grid_size_t j=0; j<size; ++j) {
-            if (grid[i][j] == 0)
+        for (grid_t *j=*i; j<&((*i)[size]); ++j) {
+            if (*j == 0)
                 printf("   │");
             else
-                printf("%3d│", grid[i][j]);
+                printf("%3d│", *j);
         }
-        if (i != size-1) {
+        if (i != &(grid[size-1])) {
             printf("\n├");
             for (grid_size_t j=0; j<size-1; ++j)
                 printf("───┼");
